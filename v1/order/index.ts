237c87a4f847e1003fdc28fb6c0ba9480475b5e6
Haruue Icymoon <haruue@caoyue.com.cn>
2017-11-22 21:30:15 +0800

    fix: add forgotten router

--- v1/order/index.ts
+++ v1/order/index.ts
@@ -1,6 +1,11 @@
 import {Router} from "express";
 import {router as list} from "./list";
+import {router as create} from "./create";
+import {router as cancel} from "./cancel";
 
 export let router = Router();
 
 router.use('/list', list);
+router.use('/create', create);
+router.use('/cancel', cancel);
+
